91a11a6bb633515381116970688c4817af0b2785
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2024-06-08 01:31:36 +0530

    chore: Update .gitignore to ignore the 'sessions' directory and remove unnecessary whitespace

--- src/alt-whatsapp/alt-whatsapp.service.ts
+++ src/alt-whatsapp/alt-whatsapp.service.ts
@@ -1,12 +1,40 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { Client, LocalAuth } from 'whatsapp-web.js';
+import { Client, LocalAuth, RemoteAuth } from 'whatsapp-web.js';
 import * as qrcode from 'qrcode-terminal';
+import { MongoStore } from 'wwebjs-mongo';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class AltWhatsappService {
   private clients: { [key: string]: Client } = {};
 
-  // register client id with whatsapp and generate qr code 
+
+  constructor() {
+    this.initializeMongoConnection();
+  }
+
+  private async initializeMongoConnection() {
+    if (mongoose.connection.readyState === 1) {
+      console.log('MongoDB connection already established');
+    } else {
+      mongoose.connection.once('open', () => {
+        console.log('MongoDB connection established');
+      });
+
+      mongoose.connection.on('error', (err) => {
+        console.error('MongoDB connection error:', err);
+      });
+
+      if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(
+          process.env.ENV === 'Local'
+            ? 'mongodb://0.0.0.0:27017/DevWhatsTest'
+            : process.env.DB_Prod_URL
+        );
+      }
+    }
+  }
+
   async registerClient(body, res): Promise<void> {
     const { clientId } = body;
     console.log(clientId);
@@ -17,25 +45,88 @@ export class AltWhatsappService {
       throw new BadRequestException(`Client ${clientId} already exists`);
     }
 
+    if (mongoose.connection.readyState !== 1) {
+      throw new BadRequestException('MongoDB connection is not established');
+    }
+
+    const store = new MongoStore({ mongoose: mongoose, session: clientId });
+    const sessionExists = await store.sessionExists({ session: clientId });
+
+    if (!sessionExists) {
+      console.log(`Session for ${clientId} does not exist. Initializing new session...`);
+
+      const client = new Client({
+        puppeteer: {
+          headless: true,
+        },
+        authStrategy: new RemoteAuth({
+          store: store,
+          // dataPath: `./sessions/${clientId}`,
+          backupSyncIntervalMs: 60 * 1000,
+        }),
+        webVersionCache: {
+          type: 'remote',
+          remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+        },
+      });
+
+      client.on('qr', (qr) => {
+        console.log(`QR RECEIVED for ${clientId}`);
+        qrcode.generate(qr, { small: true });
+      });
+
+      client.on('ready', async () => {
+        console.log(`Client ${clientId} is ready!`);
+        const version = await client.getWWebVersion();
+        console.log(`WhatsApp Web Version: ${version}`);
+      });
+
+      client.on('authenticated', () => {
+        console.log(`Client ${clientId} authenticated`);
+      });
+
+      client.on('message', async (message) => {
+        try {
+          if (message.from !== 'status@broadcast') {
+            const contact = await message.getContact();
+            console.log(`Message from ${contact.number}: ${message.body}`);
+          }
+        } catch (error) {
+          console.error(`Error handling message: ${error.message}`);
+        }
+      });
+
+      client.on('auth_failure', (msg) => {
+        console.error(`Authentication failure for ${clientId}: ${msg}`);
+      });
+
+      client.on('disconnected', (reason) => {
+        console.log(`Client ${clientId} was logged out: ${reason}`);
+        delete this.clients[clientId];
+      });
+
+      this.clients[clientId] = client;
+      await client.initialize();
+    } else {
+      console.log(`Session for ${clientId} already exists. Reinitializing client...`);
+      await this.initializeClient(store, clientId);
+    }
+  }
+
+  private async initializeClient(store: typeof MongoStore, clientId: string) {
     const client = new Client({
       puppeteer: {
         headless: true,
       },
-      authStrategy: new LocalAuth({
-        clientId: clientId,
+      authStrategy: new RemoteAuth({
+        store: store,
+        dataPath: `./sessions/${clientId}`,
+        backupSyncIntervalMs: 60 * 1000,
       }),
-
-      // to update env
       webVersionCache: {
         type: 'remote',
-        remotePath:
-          'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
       },
-    });
-
-    client.on('qr', (qr) => {
-      console.log(`QR RECEIVED for ${clientId}`);
-      qrcode.generate(qr, { small: true });
     });
 
     client.on('ready', async () => {
@@ -48,8 +139,6 @@ export class AltWhatsappService {
       console.log(`Client ${clientId} authenticated`);
     });
 
-
-    // will do it later for make whatsapp bot 
     client.on('message', async (message) => {
       try {
         if (message.from !== 'status@broadcast') {
@@ -73,7 +162,6 @@ export class AltWhatsappService {
     this.clients[clientId] = client;
     await client.initialize();
   }
-
 
   // send meesage to whatspp number
   async sendMessage(body, res) {
